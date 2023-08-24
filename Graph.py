6c10a38f347338ffb93fee2d1261e1034bc36d16
szapeta <2375894860101@ingenieria.usac.edu.gt>
2023-08-24 08:42:49 -0600

    se crea la funcionalidad de pila y pila binaria

--- Graph.py
+++ Graph.py
@@ -1,8 +1,9 @@
 import graphviz
 
 class Graph():
-    def __init__(self):
-        self.dot = graphviz.Digraph('structs', filename='structs.gv', node_attr={'shape': 'record', 'fontname':'Helvetica'})    
+    def __init__(self, nombreArchivo):
+        self.nombreArchivo = nombreArchivo
+        self.dot = graphviz.Digraph('structs', filename=f'{self.nombreArchivo}.gv', node_attr={'shape': 'record', 'fontname':'Helvetica'})    
 
     def add(self, nodoInicio, nodoSiguiente):
         if(nodoSiguiente != None):
@@ -11,5 +12,5 @@ class Graph():
             self.dot.edge(str(nodoInicio.getId()), str(nodoSiguiente.getId()))
 
     def generar(self):
-       self.dot.render(outfile='img/structs.png').replace('\\', '/')
-       'img/structs.png' 
+       self.dot.render(outfile=f'img/{self.nombreArchivo}.png').replace('\\', '/')
+       f'img/{self.nombreArchivo}.png' 
